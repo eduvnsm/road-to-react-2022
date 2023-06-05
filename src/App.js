17bb150547ac70109e2870babae88a8838f99053
Eduardo V. Micami <eduvnsm@gmail.com>
2023-06-05 13:27:17 -0300

    React State

--- src/App.js
+++ src/App.js
@@ -21,6 +21,8 @@ const stories = [
 
 const App = () => {
 
+    console.log('Rendering App Component');
+
     return (
         <div>
             <h1>My Hacker Stories</h1>
@@ -37,38 +39,54 @@ const App = () => {
 
 const Search = () => {
 
+    console.log('Rendering Search Component');
+
+    const [searchTerm, setSearchTerm] = React.useState('');
+
     const handleChange = (e) => {
-        console.log(e);
-        console.log(e.target.value);
+        setSearchTerm(e.target.value);
     };
 
     return (
         <div>
             <label htmlFor="search" placeholder="Search">Search: </label>
             <input id="search" type="text" onChange={handleChange} />
+
+            <p>
+                Search for: <strong>{searchTerm}</strong>
+            </p>
         </div>
-    )
+    );
 }
 
-const Item = (props) => (
-    <li key={props.item.objectID} style={{paddingTop: '1em'}}>
-        <span>
-            <a href={props.item.url}>
-                {props.item.title}
-            </a>
-        </span><br />
-        <span>{props.item.author}</span><br />
-        <span>{props.item.num_comments}</span><br />
-        <span>{props.item.points}</span>
-    </li>
-);
+const Item = (props) => {
+    console.log('Rendering Item Component');
 
-const List = (props) => (
-    <ul>
-        {props.list.map((item) => (
-            <Item key={item.objectID} item={item} />
-        ))}
-    </ul>
-);
+    return (
+        <li key={props.item.objectID} style={{paddingTop: '1em'}}>
+            <span>
+                <a href={props.item.url}>
+                    {props.item.title}
+                </a>
+            </span><br />
+            <span>{props.item.author}</span><br />
+            <span>{props.item.num_comments}</span><br />
+            <span>{props.item.points}</span>
+        </li>
+    );
+}
+
+const List = (props) => {
+
+    console.log('Rendering List Component');
+
+    return (
+        <ul>
+            {props.list.map((item) => (
+                <Item key={item.objectID} item={item} />
+            ))}
+        </ul>
+    );
+}
 
 export default App;
