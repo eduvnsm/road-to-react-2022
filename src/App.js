47d5d747697983604612c1f2542bdf5cfa9f2b83
Eduardo V. Micami <eduvnsm@gmail.com>
2023-06-05 23:52:38 -0300

    React Custom Hooks (Advanced)

--- src/App.js
+++ src/App.js
@@ -1,5 +1,17 @@
 import * as React from 'react';
 
+const useStorageState = (key, initialState) => {
+    const [value, setValue] = React.useState(
+        localStorage.getItem(key) || initialState
+    );
+
+    React.useEffect(() => {
+        localStorage.setItem(key, value);
+    }, [value, key]);
+
+    return [value, setValue];
+};
+
 const App = () => {
 
     const stories = [
@@ -21,20 +33,15 @@ const App = () => {
         }
     ];
 
-    const [searchTerm, setSearchTerm] = React.useState(
-        localStorage.getItem('search') || 'React'
-    );
-
-    React.useEffect(() => {
-        // Store the recent search from the browser's local storage
-        localStorage.setItem('search', searchTerm);
-    }, [searchTerm]);
+    const [searchTerm, setSearchTerm] = useStorageState('search', 'React');
 
     const handleSearch = (e) => {
         setSearchTerm(e.target.value);
     }
 
-    const searchedStories = stories.filter((story) => story.title.toLowerCase().includes(searchTerm.toLowerCase()));
+    const searchedStories = stories.filter((story) =>
+        story.title.toLowerCase().includes(searchTerm.toLowerCase())
+    );
 
     return (
         <div>
