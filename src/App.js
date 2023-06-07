9981cf120ecd9a6655f97083d68165baa114c8c3
Eduardo V. Micami <eduvnsm@gmail.com>
2023-06-07 16:05:17 -0300

    React Advanced State

--- src/App.js
+++ src/App.js
@@ -39,11 +39,25 @@ const getAsyncStories = () =>
         )
     );
 
+const storiesReducer = (state, action) => {
+    switch (action.type) {
+        case 'SET_STORIES':
+            return action.payload;
+        case 'REMOVE_STORY':
+            return state.filter(
+                (story) => action.payload.objectID !== story.objectID
+            );
+        default:
+            throw new Error();
+    }
+};
+
 const App = () => {
 
     const [searchTerm, setSearchTerm] = useStorageState('search', 'React');
 
-    const [stories, setStories] = React.useState([]);
+    const [stories, dispatchStories] = React.useReducer(storiesReducer, []);
+
     const [isLoading, setIsLoading] = React.useState(false);
     const [isError, setIsError] = React.useState(false);
 
@@ -52,7 +66,10 @@ const App = () => {
 
         getAsyncStories()
             .then(result => {
-                setStories(result.data.stories);
+                dispatchStories({
+                    type: 'SET_STORIES',
+                    payload: result.data.stories
+                });
                 setIsLoading(false);
             })
             .catch(() => setIsError(true));
@@ -60,11 +77,15 @@ const App = () => {
 
 
     const handleRemoveStory = (item) => {
+
         const newStories = stories.filter(
             (story) => item.objectID !== story.objectID
         );
 
-        setStories(newStories);
+        dispatchStories({
+            type: 'SET_STORIES',
+            payload: newStories
+        });
     };
 
     const handleSearch = (e) => {
